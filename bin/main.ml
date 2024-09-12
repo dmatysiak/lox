3d5a77580688fca6c950c771283020f4b707b96c
Daniel A. Matysiak <daniel.matysiak@droitfintech.com>
2024-09-12 19:11:29 -0400

    bkp

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,37 @@
-let () = print_endline "Hello, World!"
+(*
+   let run source =
+   let tokens = Scanner.scan_tokens in
+   List.iter (fun token -> print_endline token) tokens
+*)
+
+let run = print_endline
+
+let run_prompt _ =
+  while true do
+    print_string "> ";
+    let input = read_line () in
+    run input
+  done
+;;
+
+let run_file filename =
+  let channel = open_in filename in
+  try
+    let line = really_input_string channel (in_channel_length channel) in
+    run line;
+    flush stdout;
+    close_in channel
+  with
+  | e ->
+    close_in_noerr channel;
+    raise e
+;;
+
+let () =
+  match Array.length Sys.argv with
+  | 0 -> run_prompt ()
+  | 2 -> run_file (Array.get Sys.argv 1)
+  | _ ->
+    print_endline "Usage: lox [script]";
+    exit 64
+;;
